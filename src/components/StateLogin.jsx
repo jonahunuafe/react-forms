df4300cceb9fb28d604bc34f43bb4bf1659365ac
Jonah Unuafe <fejisfejiro@gmail.com>
2024-01-31 16:11:05 +0100

    input validation on every keystroke and lost focus

--- src/components/StateLogin.jsx
+++ src/components/StateLogin.jsx
@@ -9,20 +9,34 @@ export default function Login() {
     password: ""
   });
 
+  const [didEdit, setDidEdit] = useState({
+    email: false, 
+    password: false
+  });
+
+  const emailIsInvalid = didEdit.email && !enteredValues.email.includes("@");
+
   function handleSubmit(event) {
     event.preventDefault();
 
     console.log(enteredValues);
-    setEnteredValues({
-      email: "",
-      password: ""
-    });
   }
 
   function handleInputChange(identifier, value) {     //Generic event handling function
     setEnteredValues((prevValues) => ({
       ...prevValues,
       [identifier]: value
+    }));
+    setDidEdit((prevEdit) => ({
+      ...prevEdit,
+      [identifier]: false
+    }))
+  }
+
+  function handleInputBlur(identifier) {
+    setDidEdit(prevEdit => ({
+      ...prevEdit,
+      [identifier]: true
     }));
   }
 
@@ -45,11 +59,15 @@ export default function Login() {
             id="email" 
             type="email" 
             name="email"
+            onBlur={() => handleInputBlur("email")}
             onChange={(event) => handleInputChange("email", event.target.value)}
             value={enteredValues.email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid email address.</p>}
+          </div>
         </div>
-
+      
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
           <input 
