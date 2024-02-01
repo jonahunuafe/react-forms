49d1671f3116278056ebb560849e98cdb4eb885b
Jonah Unuafe <fejisfejiro@gmail.com>
2024-02-01 10:04:10 +0100

    validating input upon form submission

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,8 @@
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 export default function Login() {
+  const [emailIsInvalid, setEmailIsInvalid] = useState(false);
+
   const email = useRef();
   const password = useRef();
 
@@ -11,7 +13,14 @@ export default function Login() {
     const enteredEmail = email.current.value;
     const enteredPassword = password.current.value;
 
-    console.log(enteredEmail, enteredPassword);
+    const emailIsValid = enteredEmail.includes("@");
+
+    if(!emailIsValid) {
+      setEmailIsInvalid(true);
+      return;
+    }
+
+    setEmailIsInvalid(false);
   }
 
   
@@ -28,6 +37,9 @@ export default function Login() {
             name="email"
             ref={email}
           />
+          <div className="control-error">
+            {emailIsInvalid && <p>Please enter a valid enail address.</p>}
+          </div>
         </div>
 
         <div className="control no-margin">
